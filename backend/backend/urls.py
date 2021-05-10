1827fa55eae478e1a878acbcb0ae512f3bfd847c
Jeffrin Jacob <jeffrinjacob02@gmail.com>
2021-05-10 20:02:58 +0530

    edited url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from backend.app.views import *
+from app.views import *
 
 
 
