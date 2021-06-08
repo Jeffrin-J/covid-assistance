22dee2186c875e7c5e7b5070268a5fafadec5280
murugappan m <murugu2001@gmail.com>
2021-06-08 19:18:27 +0530

    fix: static root

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,14 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = "/static"
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000'
+     'http://localhost:3000', 'https:murugu-21.github.io'
 ]
 
 CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
